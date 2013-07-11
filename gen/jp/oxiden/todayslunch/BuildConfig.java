40cea3d29b20425b99947946dabc63263ed87986
OXIDEN <oxiden.dd@gmail.com>
2013-07-11 22:17:46 +0900

    add: android widget skelton

---
+++ gen/jp/oxiden/todayslunch/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jp.oxiden.todayslunch;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/jp/oxiden/todayslunch/TodaysLunchWidget.java
@@ -0,0 +1,7 @@
+package jp.oxiden.todayslunch;
+
+import android.appwidget.AppWidgetProvider;
+
+public class TodaysLunchWidget extends AppWidgetProvider {
+
+}
